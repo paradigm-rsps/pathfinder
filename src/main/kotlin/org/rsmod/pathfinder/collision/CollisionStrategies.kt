3560d6b095b0c1bc25c8aea3ebe83d17b9088cb5
Kris <kristo190@hotmail.com>
2022-03-16 15:13:16 +0200

    Add a Fly collision strategy.

--- src/main/kotlin/org/rsmod/pathfinder/collision/CollisionStrategies.kt
+++ src/main/kotlin/org/rsmod/pathfinder/collision/CollisionStrategies.kt
@@ -5,7 +5,7 @@ package org.rsmod.pathfinder.collision
 import org.rsmod.pathfinder.flag.CollisionFlag
 
 public object CollisionStrategies {
-
     public val Normal: CollisionStrategy = NormalBlockFlagCollision()
     public val Swim: CollisionStrategy = InverseBlockFlagCollision(CollisionFlag.FLOOR)
+    public val Fly: CollisionStrategy = LineOfSightBlockFlagCollision()
 }
--- src/main/kotlin/org/rsmod/pathfinder/collision/CollisionStrategy.kt
+++ src/main/kotlin/org/rsmod/pathfinder/collision/CollisionStrategy.kt
@@ -1,5 +1,7 @@
 package org.rsmod.pathfinder.collision
 
+import org.rsmod.pathfinder.flag.CollisionFlag
+
 public interface CollisionStrategy {
 
     public fun canMove(tileFlag: Int, blockFlag: Int): Boolean
@@ -17,10 +19,39 @@ public class InverseBlockFlagCollision(
 ) : CollisionStrategy {
 
     override fun canMove(tileFlag: Int, blockFlag: Int): Boolean {
-        if (inverseFlag == 0) {
-            return (tileFlag and blockFlag) == 0
-        }
         val flag = blockFlag and inverseFlag.inv()
         return (tileFlag and flag) == 0 && (tileFlag and inverseFlag) != 0
+    }
+}
+
+public class LineOfSightBlockFlagCollision : CollisionStrategy {
+
+    override fun canMove(tileFlag: Int, blockFlag: Int): Boolean {
+        val movementFlags = (blockFlag and BLOCK_MOVEMENT) shl 9
+        val routeFlags = (blockFlag and BLOCK_ROUTE) shr 13
+        val finalBlockFlag = movementFlags or routeFlags
+        return (tileFlag and finalBlockFlag) == 0
+    }
+
+    private companion object {
+        private const val BLOCK_MOVEMENT = CollisionFlag.WALL_NORTH_WEST or
+            CollisionFlag.WALL_NORTH or
+            CollisionFlag.WALL_NORTH_EAST or
+            CollisionFlag.WALL_EAST or
+            CollisionFlag.WALL_SOUTH_EAST or
+            CollisionFlag.WALL_SOUTH or
+            CollisionFlag.WALL_SOUTH_WEST or
+            CollisionFlag.WALL_WEST or
+            CollisionFlag.OBJECT
+
+        private const val BLOCK_ROUTE = CollisionFlag.WALL_NORTH_WEST_ROUTE_BLOCKER or
+            CollisionFlag.WALL_NORTH_ROUTE_BLOCKER or
+            CollisionFlag.WALL_NORTH_EAST_ROUTE_BLOCKER or
+            CollisionFlag.WALL_EAST_ROUTE_BLOCKER or
+            CollisionFlag.WALL_SOUTH_EAST_ROUTE_BLOCKER or
+            CollisionFlag.WALL_SOUTH_ROUTE_BLOCKER or
+            CollisionFlag.WALL_SOUTH_WEST_ROUTE_BLOCKER or
+            CollisionFlag.WALL_WEST_ROUTE_BLOCKER or
+            CollisionFlag.OBJECT_ROUTE_BLOCKER
     }
 }
